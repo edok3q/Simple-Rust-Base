eab84cfa654b93f8f12b38e49d92d196db11cb20
edok3q <139862149+edok3q@users.noreply.github.com>
2023-11-27 04:19:46 +0300

    Update DllMain.cpp

--- Main/DllMain.cpp
+++ Main/DllMain.cpp
@@ -18,8 +18,8 @@ auto DllMain(void*, std::uint32_t call_reason, void*) -> bool {
 	il2cpp_lib::init();
 
 	/*Init hooks*/
-	impl::hooks::ddraw_ongui.setup(xrs("UnityEngine::DDraw.OnGUI()"), &impl::hooks::hk_ddraw_ongui, 0); 
-	impl::hooks::bp_client_input.setup(xrs("BasePlayer.ClientInput()"), &impl::hooks::hk_bp_client_input); 
+	impl::hooks::ddraw_ongui.setup(xrs("UnityEngin3::DDraw.OnGUI()"), &impl::hooks::hk_ddraw_ongui, 0); 
+	impl::hooks::bp_client_input.setup(xrs("Bas3Play3r.ClientInput()"), &impl::hooks::hk_bp_client_input); 
 
 	/*Beep After Inject*/
 	Beep(330, 100);
